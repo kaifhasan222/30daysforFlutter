ba6e82da5851feb6da27548a9343fb384d64d416
Kaif Hasan <kaifhasan222@gmail.com>
2022-09-20 15:18:25 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/Home_page.dart';
+import 'package:new_app/pages/Home_page.dart';
+import 'package:new_app/pages/loginPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Subhamita";
-
-  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(

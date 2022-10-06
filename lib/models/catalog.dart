32f166448dd9d747dd5586ce576be889ecb2a10e
Kaif Hasan <kaifhasan222@gmail.com>
2022-10-06 12:29:07 +0530

    day11 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,5 @@
 class CatalogModels {
-  static final Items = [
+  static List<Item> Items = [
     Item(
         id: 1,
         name: "Iphone 12 pro max",
@@ -26,4 +26,24 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image,
+      };
 }
--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -28,11 +28,14 @@ class _HomePageState extends State<HomePage> {
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
     var productData = decodedData["products"];
+    CatalogModels.Items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModels.Items[0]);
+    // final dummyList = List.generate(20, (index) => CatalogModels.Items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -42,14 +45,18 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModels.Items != null && CatalogModels.Items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModels.Items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatalogModels.Items[index],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
